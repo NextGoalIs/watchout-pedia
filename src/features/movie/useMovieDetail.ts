cf1e4a8d29ca3372a4d192dbd2f46a111e36e6e8
NextGoalIs <wlswls8954@gmail.com>
2022-07-14 14:12:49 +0900

    make movie detail page

---
+++ src/features/movie/useMovieDetail.ts
@@ -0,0 +1,15 @@
+import { AxiosError, AxiosResponse } from "axios";
+import { useQuery } from "react-query";
+import { detailApi } from "../../apis/movieApi";
+import { MovieDetail } from "../../types";
+
+const useMovieDetail = (query: string) => {
+  const queryFn = () => detailApi(query);
+
+  return useQuery<AxiosResponse<MovieDetail>, AxiosError>(
+    ["movieDetail", query],
+    queryFn
+  );
+};
+
+export default useMovieDetail;
